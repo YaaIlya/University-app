7823318f348ea41784bfebc8f7e1f30bc7eb44ba
YaaIlya <118045936+YaaIlya@users.noreply.github.com>
2024-02-01 19:14:56 +0300

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return <div className='footer'>
+        All rights are okey
+    </div>
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://edu.stankin.ru/pluginfile.php/1/theme_opentechnology/settings_stankinc_header_logoimage/1704896864/Z3%20%284%29.png' />
+    </header>
+}
+
+export default Header;
