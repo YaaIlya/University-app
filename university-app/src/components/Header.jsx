8fc1c2f4224f15810b501064ba067b34a1e011be
YaaIlya <ilyaz3007@gmail.com>
2024-02-07 19:44:23 +0300

    added files with styles for components 	new file:   university-app/src/components/Content.jsx 	new file:   university-app/src/components/Footer.css

---
+++ university-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Header.css";
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://edu.stankin.ru/pluginfile.php/1/theme_opentechnology/settings_stankinc_header_logoimage/1704896864/Z3%20%284%29.png' />
+    </header>
+}
+
+export default Header;
