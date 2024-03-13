3f69b0b3d596577a500ca559f435fa5307cf8de3
YaaIlya <ilyaz3007@gmail.com>
2024-03-13 14:52:26 +0300

    Update

--- university-app/src/components/Users/Users.jsx
+++ university-app/src/components/Users/Users.jsx
@@ -5,16 +5,20 @@ import userPhoto from "../../assets/images/user.png";
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
 
 
+
     return <div className={styles.info}>
+        <button onClick={getUsers}>Get users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
